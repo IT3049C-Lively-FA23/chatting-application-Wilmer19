68e8bdc2d5d66731f1783b014e508a327c9f2f24
Wilmer19 <40205328+Wilmer19@users.noreply.github.com>
2023-09-21 10:56:57 -0400

    Create a formatter function and rearranged updateMessage() and fetchMessages() function

--- resources/js/index.js
+++ resources/js/index.js
@@ -4,17 +4,47 @@ const myMessage = document.getElementById("my-message");
 const sendButton = document.getElementById("send-button");
 const chatBox = document.getElementById("chat");
 
-function updateMessages() {
-  // Fetch Messages
-  // Loop over the messages. Inside the loop we will
-      // get each message
-      // format it
-      // add it to the chatbox
-}
-
 const serverURL = `https://it3049c-chat.fly.dev/messages`;
 
 function fetchMessages() {
     return fetch(serverURL)
         .then( response => response.json())
+}
+
+function formatMessage(message, myNameInput) {
+  const time = new Date(message.timestamp);
+  const formattedTime = `${time.getHours()}:${time.getMinutes()}`;
+
+  if (myNameInput === message.sender) {
+      return `
+      <div class="mine messages">
+          <div class="message">
+              ${message.text}
+          </div>
+          <div class="sender-info">
+              ${formattedTime}
+          </div>
+      </div>
+      `
+  } else {
+      return `
+          <div class="yours messages">
+              <div class="message">
+                  ${message.text}
+              </div>
+              <div class="sender-info">
+                  ${message.sender} ${formattedTime}
+              </div>
+          </div>
+      `
+  }
+}
+
+async function updateMessages() {
+  // Fetch Messages
+  const messages = await fetchMessages();
+  // Loop over the messages. Inside the loop we will
+      // get each message
+      // format it
+      // add it to the chatbox
 }
